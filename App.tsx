39db700e9bc25330b83ff668bf9ebaa72aac9e0d
Gabriel dos Anjos <gabrieldosanjosdbz>
2024-09-12 08:53:53 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { StyledProvider } from "@gluestack-style/react"
+import { OverlayProvider } from "@gluestack-ui/overlay"
+import { ToastProvider } from "@gluestack-ui/toast"
+import { config, createProvider } from '@gluestack-ui/themed-native-base';
+
+const GluestackUIStyledProvider = createProvider({ StyledProvider })
 
 export default function App() {
   return (
-    <View>
-      <StatusBar style="auto" />
-      <Text>Funciona!</Text>
-    </View>
+    <GluestackUIStyledProvider config={config}>
+      <OverlayProvider>
+        <ToastProvider></ToastProvider>
+      </OverlayProvider>
+    </GluestackUIStyledProvider>
   );
 }
