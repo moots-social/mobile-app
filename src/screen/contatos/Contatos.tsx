5e32af96e2f2e3db8f10ae488ff7cfcf57883376
sntpwbl <joaop401@outlook.com>
2024-09-26 19:26:14 -0300

    att bottom tabs

--- src/screen/contatos/Contatos.tsx
+++ src/screen/contatos/Contatos.tsx
@@ -1,8 +1,6 @@
 
 import { Box, Text } from '@gluestack-ui/themed'
-import CabecalhoUsuario from '../../components/CabecalhoUsuario'
 import Contato from '../../components/Contato'
-import Navbar from '../../components/Navbar'
 
 export default function Contatos() {
   return <Box alignItems="center">
