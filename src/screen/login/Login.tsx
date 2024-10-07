95339220b8c702bfe692022aedf820ac510dc71a
Gabriel dos Anjos <gabrieldosanjosdbz>
2024-10-07 11:54:32 -0300

    ola

--- src/screen/login/Login.tsx
+++ src/screen/login/Login.tsx
@@ -4,6 +4,9 @@ import { ButtonText, Button } from "@gluestack-ui/themed";
 import { Titulo, TextoNegrito } from "../../components/Texto";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { useEffect, useState } from "react";
+import { usuarioLogin } from "../../api/apis";
+import axios from "axios";
+
 
 const StyledVStack = styled(VStack, {
   display: "flex",
@@ -14,18 +17,40 @@ const StyledVStack = styled(VStack, {
 });
 
 export default function Login({ navigation }) {
-  const [nome, setNome] = useState<string>("");
   const [email, setEmail] = useState<string>("");
+  const [senha, setSenha] = useState<string>("");
   const [resp, setResp] = useState<string>("");
+  
+  const handleSubmit = async () => {
+    try {
+        const dado = await usuarioLogin.post("", {
+            email : email,
+            senha : senha
+        });
+
+        const res = await dado.data;
+        
+        if(res)
+          setResp("login feito com sucesso" + res.token);
+        else{
+          setResp("deu certo nao boy")
+        }
+    } catch (error) {
+      console.error("Erro no login:", error);
+      setResp("Erro ao realizar login");
+  }
+};
 
   return (
     <StyledVStack>
       <Box bg="white" borderTopLeftRadius={50} borderTopRightRadius={50} width="100%" height="50%" alignItems="center">
         <Titulo mt={5} mg={5}> Realize o login e aproveite</Titulo>
 
+        <Text>{resp}</Text>
+
         <Box alignItems="center" mb={30} w="80%">
           <FormControl w="100%">
-            <Input  placeholder="Digite seu email"  borderRadius={30}  textAlign="center"  fontFamily="Poppins_500Medium"/>
+            <Input  placeholder="Digite seu email"  borderRadius={30}  textAlign="center"  fontFamily="Poppins_500Medium" onChange={(text) => setEmail(text)}/>
 
             <Box flexDirection="row" mt={5} mb={5}>
               <TextoNegrito>Não tem uma conta? </TextoNegrito>
@@ -34,7 +59,7 @@ export default function Login({ navigation }) {
               </TouchableOpacity>
             </Box>
 
-            <Input  placeholder="Digite sua senha"  borderRadius={30}  textAlign="center"  fontFamily="Poppins_500Medium" />
+            <Input  placeholder="Digite sua senha"  borderRadius={30}  textAlign="center"  fontFamily="Poppins_500Medium" onChange={(text) => setSenha(text)}/>
             <Box flexDirection="row" mt={5} mb={5}>
               <TextoNegrito>Esqueceu sua senha?</TextoNegrito>
               <TouchableOpacity onPress={() => {navigation.navigate("cadastro")}}>
@@ -47,7 +72,7 @@ export default function Login({ navigation }) {
         <Box alignItems="center" w="80%">
           
           <Button  w="100%" borderStyled="solid"  borderWidth={1}  borderColor="black"  bg="#AF75BFBA"  h={50}  borderRadius={40}
-          onPress={()=>navigation.navigate('tabs')}>
+          onPress={() => handleSubmit()}>
             <ButtonText fontFamily="Poppins_700Bold">Iniciar Sessão</ButtonText>
           </Button>
         </Box>
