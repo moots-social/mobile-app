27d1425064e1e93e728bc2bc3ccf1f6908ac3608
sntpwbl <joaop401@outlook.com>
2024-10-18 11:44:48 -0300

    fix/excluir-conta

--- src/screen/editarPerfil/ExcluirConta.tsx
+++ src/screen/editarPerfil/ExcluirConta.tsx
@@ -6,17 +6,17 @@ import { TextoNegrito } from "../../components/Texto";
 
 export default function ExcluirConta(){
     return(
-        <LinearGradientMoots>
+        <Box w="100%" bg="$white" h="100%">
             <CabecalhoPerfil titulo='Excluir conta' />
-            <RoundedBottom w="100%" bg="$white" bottom={5} h={625} alignItems="center">
-                <Box w="90%" alignItems="center" my={20} >
+            <Box w="100%" bg="$white" h="85%" justifyContent="space-between" alignItems="center">
+                <Box w="90%" alignItems="center" mt={20} >
                     <TextoNegrito textAlign="center">Se você excluir sua conta, seu email ficará livre para a criação de outra conta. Além disso, você perderá suas publicações salvas, informações do perfil e suas conversas.</TextoNegrito>
                     <TextoNegrito mt={35}>Essa ação não pode ser revertida.</TextoNegrito>
                 </Box>
-                <Box justifyContent="flex-end" h="63%" py={35} alignItems="center">
+                <Box justifyContent="flex-end" alignItems="center">
                     <Text fontFamily="Poppins_600SemiBold" fontSize={24} color="#FF2626">Excluir conta</Text>
                 </Box>
-            </RoundedBottom>
-        </LinearGradientMoots>
+            </Box>
+        </Box>
     )
 }
