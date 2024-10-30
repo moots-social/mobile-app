7d5c25b9d2f6bffe8981046ea9e04bfde13b297a
sntpwbl <joaop401@outlook.com>
2024-10-29 23:42:17 -0300

    feat/search-api

--- src/api/apis.ts
+++ src/api/apis.ts
@@ -4,14 +4,22 @@ export const usuarioApi = axios.create({
     baseURL: "http://192.168.15.8:8080/user",
     headers: {
         Accept: "application/json",
-        "Content-Type": "application/json" // Adicionando Content-Type
+        "Content-Type": "application/json" 
     }
 });
 
 export const usuarioLogin = axios.create({
     baseURL: "http://192.168.15.8:8080/login",
     headers: {
-        Accept: "application/json", // Corrigido de "aplication/json"
-        "Content-Type": "application/json" // Adicionando Content-Type
+        Accept: "application/json",
+        "Content-Type": "application/json" 
+    }
+});
+
+export const searchApi = axios.create({
+    baseURL: "http://192.168.15.8:8080/search", // ou /user ou /post
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
     }
 });
